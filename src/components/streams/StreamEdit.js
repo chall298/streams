de692248b868a16f213673fc53f065bf40d710b5
chall298 <chall361@gmail.com>
2021-04-21 12:05:02 -0400

    made a streamForm component for edit & create view

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
     }
 
+    onSubmit = (formValues) => {
+        console.log(formValues)
+    };
+
     render () {
-        console.log(props);
+        if (!this.props.stream) {
+        return <div>Loading...</div>
+        }
+
         return (
-            <div>StreamEdit</div>
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm 
+                    initialValues={this.props.stream} 
+                    onSubmit={this.onSubmit} 
+                />
+            </div>
         );
     }
 };
@@ -18,4 +33,4 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
