a4ea2f2ee043bd478c12b82d5d41e3df87524ec8
chall298 <chall361@gmail.com>
2021-04-23 12:24:45 -0400

    Added props to modal to be used 4 other components

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Modal from '../Modal'
+import history from '../../history';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
-    return (
-        <div>
-            StreamDelete
-        </div>
-    );
-};
+class StreamDelete extends React.Component {
+    componentDidMount() {
+        // console.log(this.props)
+        this.props.fetchStream(this.props.match.params.id)
+    }
 
-export default StreamDelete;
+    renderActions() {
+        return (
+            <>
+                <button className="ui button negative">Delete</button>
+                <button className="ui button">Cancel</button>
+            </>
+        );
+    }
+    
+    render () {
+        return (
+            <div>
+                StreamDelete
+                <Modal
+                    title="Delete Stream"
+                    content="Are you sure you want to delete this stream?"
+                    actions={this.renderActions()}
+                    onDismiss={() => history.push('/')}
+                />
+            </div>
+        );
+    }
+    
+}
+
+export default connect(null, { fetchStream })(StreamDelete);
